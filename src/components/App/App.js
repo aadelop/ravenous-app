b26b55e390800e1baebce9113886e17d55c2b997
Alfredo Delgado <aadelop@gmail.com>
2020-05-30 13:22:02 +0200

    Created Business Component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Business from '../Business/Business';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      < Business />
     </div>
   );
 }
