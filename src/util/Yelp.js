1ba264038132e92a7f4f57668b9e3246d27d9144
Alfredo Delgado <aadelop@gmail.com>
2020-06-02 10:43:45 +0200

    Added Yelp.js to connect the app with the API

---
+++ src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'QlZk6_1-XjyxbllyiNQ_pAewtxGvLcXx-hCk_5Fdl-_Y-QdQ2YExR1fyZieb51ljBUt5FbrD3-o5cwn5u2lF7_pnpuB_KDtam03LF2dElFPSSKB3iyl9vRvXHgfWXnYx'
+const Yelp = {
+
+	search(term,location,sortBy){
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}')https://api.yelp.com/v3/businesses/search?term=TERM&location=LOCATION&sort_by=SORT_BY`,
+					 headers:{Authorization: `Bearer ${apiKey}`	
+					 })
+				.then(response =>{ return response.json()})
+				.then(jsonResponse => {
+					if (jsonResponse.businesses){
+						jsonResponse.businesses.map(business=>{
+							return {id:business.id,
+									imageSrc:business.image_url,
+									name:business.name,
+									address:business.location.address1,
+									city:business.location.city,
+									state:business.location.state,
+									zipCode:business.location.zip_code,
+									category:business.categories[0].title,
+									rating:business.rating,
+									reviewCount:business.review_count
+								}
+						})
+					}
+				})
+
+	}
+}
+
+export default Yelp;
